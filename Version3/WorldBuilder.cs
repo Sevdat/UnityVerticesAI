f7ce72d0c13dcc69b07afdba58ac6f3e102f08b3
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-02-23 22:29:48 +0100

    Update WorldBuilder.cs

--- Version3/WorldBuilder.cs
+++ Version3/WorldBuilder.cs
@@ -12,9 +12,15 @@ public class WorldBuilder : MonoBehaviour
     public TextAsset textDoc;
     public static List<int> ballLocations = new List<int>(); 
     public static Vector3[] worldDimensions;
+    int arraySize;
+    int arrayWidth;
     void Awake()
     {
         ballMesh = ball.GetComponent<MeshFilter>().mesh;
+        binaryReader();
+    }
+
+    void binaryReader(){
         using (StreamReader reader = new StreamReader("Assets/v3/binaryWorld.txt"))
         {
             int check = 0;
@@ -24,8 +30,8 @@ public class WorldBuilder : MonoBehaviour
                 if (check == zero || check == one)
                     ballLocations.Add(check);
             }
-            int arraySize = ballLocations.Count;
-            int arrayWidth = (int)Math.Cbrt(arraySize);
+            arraySize = ballLocations.Count;
+            arrayWidth = (int)Math.Cbrt(arraySize);
             worldDimensions = new Vector3[arraySize];
             float x = 0;
             float y = 0;
@@ -40,6 +46,27 @@ public class WorldBuilder : MonoBehaviour
             }
         }
     }
+    void binaryWriter(){
+        using (StreamWriter writer = new StreamWriter("Assets/v3/binaryWorld.txt"))
+        {
+            string c = "";
+            string p = "";
+            int one = 49;
+            int paragraph = 0;
+            int newTile = 0;
+            for (int i = 0; i < arraySize; i++){
+                paragraph++;
+                if (paragraph == arrayWidth) {
+                    p = $"\n"; paragraph = 0; newTile += 1; 
+                    } else p = "";
+                if (newTile == arrayWidth) {
+                    p = $"\n\n"; newTile = 0;
+                        }
+                if (ballLocations[i] == one) c = "1"; else c = "0";
+                writer.Write($"{c}{p}");
+            }
+        }
+    }
 
     int index = 0;
     void Update()
@@ -49,6 +76,7 @@ public class WorldBuilder : MonoBehaviour
             ballLocations[index] = 48;
             print(ballLocations[index+1]);
             index +=1;
+            if (index == 10) binaryWriter();
         }
     }
 }
