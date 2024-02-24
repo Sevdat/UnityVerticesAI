17b39e4384cacd848f10509213d49b1bd374e006
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-02-25 00:08:08 +0100

    Update ballVisible.cs

--- Version3/ballVisible.cs
+++ Version3/ballVisible.cs
@@ -1,36 +1,39 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+DONT USE THIS METHOD. WHEN THE NUMBERS EXCEED 10 EACH OF THEM WILL CALL
+AND CRASH UNITY. IT WAS A NICE ATTEMPT THOUGH))
 
-public class ballVisible : MonoBehaviour
-{
-    // Start is called before the first frame update
-    int index;
-    bool value;
-    bool checkOnce = true;
-    bool success;
-    void Start()
-    { 
-        success = int.TryParse(gameObject.name, out index);
-        if (success){
-        transform.position = WorldBuilder.worldDimensions[index];
-        value = WorldBuilder.bitArray[index];
-        }
-        if (success && value) 
-            GetComponent<MeshFilter>().mesh.Clear();
+// using System;
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class ballVisible : MonoBehaviour
+// {
+//     // Start is called before the first frame update
+//     int index;
+//     bool value;
+//     bool checkOnce = true;
+//     bool success;
+//     void Start()
+//     { 
+//         success = int.TryParse(gameObject.name, out index);
+//         if (success){
+//         transform.position = WorldBuilder.worldDimensions[index];
+//         value = WorldBuilder.bitArray[index];
+//         }
+//         if (success && !value) 
+//             GetComponent<MeshFilter>().mesh.Clear();
             
-    }
-    // Update is called once per frame
-    void Update()
-    {
-        if (success) value = WorldBuilder.bitArray[index];
-        if (success && checkOnce && value) {
-            GetComponent<MeshFilter>().mesh = WorldBuilder.ballMesh;
-            checkOnce = false;
-        } else if (success && !checkOnce && !value) {
-            GetComponent<MeshFilter>().mesh.Clear();
-            checkOnce = true;
-            }
-    }
-}
+//     }
+//     // Update is called once per frame
+//     void Update()
+//     {
+//         if (success) value = WorldBuilder.bitArray[index];
+//         if (success && checkOnce && value) {
+//             GetComponent<MeshFilter>().mesh = WorldBuilder.ballMesh;
+//             checkOnce = false;
+//         } else if (success && !checkOnce && !value) {
+//             GetComponent<MeshFilter>().mesh.Clear();
+//             checkOnce = true;
+//             }
+//     }
+// }
