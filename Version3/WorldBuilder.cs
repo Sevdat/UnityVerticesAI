21fe2d412ddf0d0e927ad80cb08f86cb6ad63315
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-02-24 16:40:55 +0100

    Update WorldBuilder.cs

--- Version3/WorldBuilder.cs
+++ Version3/WorldBuilder.cs
@@ -14,36 +14,41 @@ public class WorldBuilder : MonoBehaviour
     public static Vector3[] worldDimensions;
     int arraySize;
     int arrayWidth;
+    bool readAtBegin = false;
+    int zero = 48;
+    int one = 49;
+    BitArray bitArray = new BitArray(16);
+    BitArray tempByte;
     void Awake()
     {
-        ballMesh = ball.GetComponent<MeshFilter>().mesh;
+        // ballMesh = ball.GetComponent<MeshFilter>().mesh;
+        // if (readAtBegin) binaryReader(); else {
+        //     arraySize = (int)Math.Pow(arrayWidth,3f);
+        //     for (int i = 0; i< arraySize;i++){
+        //         ballLocations.Add(zero);
+        //     }
+        //     binaryWriter();
+        // }
         binaryReader();
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     void binaryReader(){
         using (StreamReader reader = new StreamReader("Assets/v3/binaryWorld.txt"))
         {
             int check = 0;
-            int zero = 48;
-            int one = 49;
+            int index = 0;
             while ((check = reader.Read()) != -1) {
-                if (check == zero || check == one)
-                    ballLocations.Add(check);
+                for(int i = 0; i<8;i++){
+                    bitArray[i + index] = 
+                        (check & 0x80) != 0 ? true:false; 
+                    check = check << 1;
+                }
+                index += 8;
             }
-            arraySize = ballLocations.Count;
-            arrayWidth = (int)Math.Cbrt(arraySize);
-            worldDimensions = new Vector3[arraySize];
-            float x = 0;
-            float y = 0;
-            float z = 0;
-            for (int i = 0; i<arraySize; i++){
-                GameObject clone = Instantiate(ball);
-                clone.name = $"{i}";
-                worldDimensions[i] = new Vector3(x,y,z);
-                x+=1;
-                if (z > arrayWidth-2 && x > arrayWidth-1) {x = 0; z = 0; y += 1;}
-                if (x > arrayWidth-1) {x = 0; z+=1;}; 
-            }
+        }
+        for (int i = 0; i<16;i++){
+            print(bitArray[i]);
         }
     }
     void binaryWriter(){
@@ -68,15 +73,45 @@ public class WorldBuilder : MonoBehaviour
         }
     }
 
+    void binaryGet(){
+        arraySize = ballLocations.Count;
+        arrayWidth = (int)Math.Cbrt(arraySize);
+        worldDimensions = new Vector3[arraySize];
+        float x = 0;
+        float y = 0;
+        float z = 0;
+        for (int i = 0; i<arraySize; i++){
+            GameObject clone = Instantiate(ball);
+            clone.name = $"{i}";
+            worldDimensions[i] = new Vector3(x,y,z);
+            x+=1;
+            if (z > arrayWidth-2 && x > arrayWidth-1) {x = 0; z = 0; y += 1;}
+            if (x > arrayWidth-1) {x = 0; z+=1;}; 
+        }
+    }
+
     int index = 0;
     void Update()
     {
-        if (Input.GetKeyDown("space"))
+        if (Input.GetKeyDown("up"))
         {
-            ballLocations[index] = 48;
-            print(ballLocations[index+1]);
-            index +=1;
-            if (index == 10) binaryWriter();
+            
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            
+        }
+        if (Input.GetKeyDown("p"))
+        {
+            binaryWriter();
         }
     }
 }
