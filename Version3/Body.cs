c36dd7570ebaa1af4f1632c81a43bc3f2ceea88f
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-05 13:01:06 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -7,11 +7,12 @@ public class Body : MonoBehaviour
     // Start is called before the first frame update
     Vector3[] chest;
     void Start(){
-        WorldBuilder.vecToInt(
-            WorldBuilder.dimensionX/2,
-            WorldBuilder.dimensionX/2,
-            WorldBuilder.dimensionZ/2
-            );
+        int x = WorldBuilder.dimensionX/2;
+        int y = WorldBuilder.dimensionY/2;
+        int z = WorldBuilder.dimensionZ/2;
+        int lol = WorldBuilder.vecToInt(x,y,z);
+        Vector3 lol2 = new Vector3(x,y,z);
+        WorldBuilder.cloneCreator(lol,lol2,true);
     }
 
     // Update is called once per frame
