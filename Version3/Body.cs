5a13ba53ec1ed7a9a1bd534326ac8d02dc0438d8
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-05 13:55:33 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
-    Vector3[] chest;
+    Vector3[] chest = new Vector3[10];
     void Start(){
-        int x = WorldBuilder.dimensionX/2;
-        int y = WorldBuilder.dimensionY/2;
-        int z = WorldBuilder.dimensionZ/2;
-        int lol = WorldBuilder.vecToInt(x,y,z);
-        Vector3 lol2 = new Vector3(x,y,z);
-        WorldBuilder.cloneCreator(lol,lol2,true);
+        for (int i = 0; i <chest.Length;i++){
+            int x = WorldBuilder.dimensionX/2;
+            int y = (WorldBuilder.dimensionY/2)+i;
+            int z = WorldBuilder.dimensionZ/2;
+            int[] a = WorldBuilder.boundry(x,y,z);
+            x = a[0];
+            y = a[1];
+            z = a[2];
+            Vector3 lol2 = new Vector3(x,y,z);
+            int lol = WorldBuilder.vecToInt(x,y,z);
+            WorldBuilder.cloneCreator(lol,lol2,true);
+        }
     }
 
     // Update is called once per frame
