c9607a60dd03269b7d1b0a889d30a097c5c1d1ee
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-07 21:20:40 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -2,22 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
-    Vector3[] chest = new Vector3[20];
-    void Start(){
-        int x = WorldBuilder.dimensionX/2;
-        int y = WorldBuilder.dimensionY/2;
-        int z = WorldBuilder.dimensionZ/2;
-        for (int i = 0; i <chest.Length;i++){
-            x = WorldBuilder.boundry(x,3,WorldBuilder.dimensionX);
-            y = WorldBuilder.boundry(y,3,WorldBuilder.dimensionY);
-            z = WorldBuilder.boundry(z,3,WorldBuilder.dimensionX);
-            int lol = WorldBuilder.vecToInt(x,y,z);
-            Vector3 lol2 = new Vector3(x,y,z);
-            WorldBuilder.cloneCreator(lol,lol2,true);
+    static Vector3[] chest = new Vector3[20];
+    class lol:WorldBuilder{
+        public static void loli(){
+            int x = dimensionX/2;
+            int y = dimensionY/2;
+            int z = dimensionZ/2;
+            for (int i = 0; i <chest.Length;i++){
+                x = boundry(x,1,dimensionX);
+                y = boundry(y,1,dimensionY);
+                z = boundry(z,1,dimensionX);
+                int lol = vecToInt(x,y,z);
+                Vector3 lol2 = new Vector3(x,y,z);
+                cloneCreator(lol,lol2,true);
+            }
         }
+    }
+    void Start(){
+        lol.loli();
     }
 
     // Update is called once per frame
