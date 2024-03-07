55da481921d800c3be90c237ddd3e3995e97d102
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-07 23:45:09 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -6,9 +6,11 @@ using UnityEngine;
 public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static int[] chest = new int[3]{4,4,4};
+    public static Vector3Int[] chest = new Vector3Int[1]{
+         new Vector3Int(4,4,4)
+         };
     void Start(){
-        WorldBuilder.move(chest,0,-1,-1,5);
+        WorldBuilder.move(chest[0],-1,-1,6);
     }
 
     // Update is called once per frame
