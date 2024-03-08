7dc67402069ec84ba647f176dd3a80a1f90f668b
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-08 23:17:02 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -6,19 +6,21 @@ using UnityEngine;
 public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
-    public static Vector3Int[] chest = new Vector3Int[1]{
-         new Vector3Int(4,4,4)
+    public static Vector3Int[] chest = new Vector3Int[]{
+         new Vector3Int(4,4,5),new Vector3Int(4,4,4),
          };
     void Start(){
-        WorldBuilder.move(chest[0],new Vector3Int(-1,-1,6),true);
+            chest[0] = WorldBuilder.createVector(chest[0],new Vector3Int(0,0,0));
+            chest[1] = WorldBuilder.createVector(chest[1],new Vector3Int(0,0,0));
     }
 
     // Update is called once per frame
     float time = 0;
     void Update(){
         time += Time.deltaTime;
-        if (time > 5f){
-            print(time);
+        if (time > 2f){
+            chest[0] = WorldBuilder.createVector(chest[0],new Vector3Int(0,0,1));
+            chest[1] = WorldBuilder.createVector(chest[1],new Vector3Int(0,0,-1));
             time = 0;
         }
     }
