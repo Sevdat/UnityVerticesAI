598ebf07383460037c16559349d9168af6e0ccf6
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-10 22:11:54 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -13,21 +13,30 @@ public class Body : MonoBehaviour
     public static Vector3Int move = new Vector3Int(0,0,1); 
     public static Vector3Int[] tempChest;     
     void Start(){
-            tempChest = chest;
     }
 
     // Update is called once per frame
     float time = 0;
+    int i = 0;
     void Update(){
         time += Time.deltaTime;
-        if (time > 0.4f){
+        if (time > 0.1f){
+            float cos = Mathf.Cos(i*Mathf.PI/180);
+            float sin = Mathf.Sin(i*Mathf.PI/180);
+            int y = (Mathf.Sign(cos)>0)?
+                    (int)(2*cos +0.5f):
+                        (int)(2*cos -0.5f);
+            int x = (Mathf.Sign(sin)>0)?
+                    (int)(2*sin +0.5f):
+                        (int)(2*sin -0.5f);
+            print($"{x}:{y}");
             WorldBuilder.createOrDelete(
                 WorldBuilder.setVectorInBoundry(
-                    chest[0],new Vector3Int(0,0,0)),false
-                    );
-            print(chest[0]);
-            chest[0] = WorldBuilder.createInBoundry(chest[0], move);;
+                    chest[0],new Vector3Int(x,y,0)
+                    ),true
+                );
             time = 0;
+            i++;
         }
     }
 }
