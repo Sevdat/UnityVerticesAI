5e5a2caface983cbec367f87c5ea07f30775212b
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-11 00:28:49 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -8,7 +8,7 @@ public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Vector3Int[] chest = new Vector3Int[]{
-         new Vector3Int(4,4,5),new Vector3Int(4,4,4)
+         new Vector3Int(4,4,7),new Vector3Int(4,4,4)
          };
     public static Vector3Int move = new Vector3Int(0,0,1); 
     public static Vector3Int[] tempChest;     
@@ -18,21 +18,31 @@ public class Body : MonoBehaviour
     // Update is called once per frame
     float time = 0;
     int i = 0;
+    Vector3Int rotate(
+        int angle,
+        Vector3Int point, Vector3Int origin
+         )
+         {
+        float radius = MathF.Sqrt(
+            Mathf.Pow(origin.x-point.x,2)+
+            Mathf.Pow(origin.y-point.y,2)+
+            Mathf.Pow(origin.z-point.z,2)
+        );
+        float sin = radius*Mathf.Sin(angle*Mathf.PI/180);
+        float cos = radius*Mathf.Cos(angle*Mathf.PI/180);
+        int x = (sin>0)? (int)(sin +0.5f):(int)(sin -0.5f);
+        int y = (cos>0)? (int)(cos +0.5f):(int)(cos -0.5f);
+        print($"{x}:{y}:{radius}");
+        return new Vector3Int(x,y,0);
+        
+    }
     void Update(){
         time += Time.deltaTime;
         if (time > 0.1f){
-            float cos = Mathf.Cos(i*Mathf.PI/180);
-            float sin = Mathf.Sin(i*Mathf.PI/180);
-            int y = (Mathf.Sign(cos)>0)?
-                    (int)(2*cos +0.5f):
-                        (int)(2*cos -0.5f);
-            int x = (Mathf.Sign(sin)>0)?
-                    (int)(2*sin +0.5f):
-                        (int)(2*sin -0.5f);
-            print($"{x}:{y}");
+
             WorldBuilder.createOrDelete(
                 WorldBuilder.setVectorInBoundry(
-                    chest[0],new Vector3Int(x,y,0)
+                    chest[0], rotate(i,chest[0],chest[1])
                     ),true
                 );
             time = 0;
