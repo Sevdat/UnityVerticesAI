7dbeac5e7338ffd5f603671aaff954957cf313bc
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-11 18:09:38 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -29,6 +29,7 @@ public class Body : MonoBehaviour
     // Update is called once per frame
     float time = 0;
     int i = 0;
+    //https://stackoverflow.com/questions/20313363/trigonometry-3d-rotation-around-center-point
     Vector3Int rotate(
         int angleXY, int angleZ,
         Vector3Int point, Vector3Int origin
@@ -40,30 +41,30 @@ public class Body : MonoBehaviour
         );
         float sin = Mathf.Sin(angleXY*Mathf.PI/180);
         float cos = Mathf.Cos(angleXY*Mathf.PI/180);
+        angleZ -= 90;
         float sinZ = Mathf.Sin(angleZ*Mathf.PI/180);
-        float cosZ = Mathf.Sin(angleZ*Mathf.PI/180);
-
-        float x = radius*sin*sinZ;
+        float cosZ = Mathf.Cos(angleZ*Mathf.PI/180);
+        float x = radius*cos*sinZ;
         int x1 = (x>0)? (int)(x +0.5f):(int)(x -0.5f);
 
-        float y = radius*cos*sinZ;
+        float y = radius*sin*sinZ;
         int y1 = (y>0)? (int)(y +0.5f):(int)(y -0.5f);
         
-        float z = radius*sinZ;
+        float z = radius*cosZ;
         int z1 = (z>0)? (int)(z +0.5f):(int)(z -0.5f);
-        print($"{x}:{y}:{radius}");
         return new Vector3Int(x1,y1,z1);
-        
     }
+    int l = -90;
     void Update(){
         time += Time.deltaTime;
         if (i<361){
             WorldBuilder.createOrDelete(
                 WorldBuilder.setVectorInBoundry(
-                    chest[0], rotate(i,-90,chest[0],chest[1])
+                    chest[0], rotate(-30,i,chest[0],chest[1])
                     ),true
                 );
-            i++;
+                i++;
+            if (i>=360 && l <90) {i =0;l+=10;};
         }
     }
 }
