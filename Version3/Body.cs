c2efc68d53a0748715d78b810630e20401b539d2
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-16 09:39:30 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 
 
@@ -8,7 +9,7 @@ public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
     public static Vector3Int[] chest = new Vector3Int[]{
-         new Vector3Int(5,3,5),new Vector3Int(3,5,10)
+         new Vector3Int(7,3,5),new Vector3Int(3,5,10)
          };
     public static Vector3Int move = new Vector3Int(0,0,1); 
     public static Vector3Int[] tempChest;     
@@ -26,95 +27,67 @@ public class Body : MonoBehaviour
     }
 
     // Update is called once per frame
-    float time = 0;
-    int i = 0;
-
-    Vector3Int rotateZX(
+        float time = 0;
+        int i = 0;
+        const int rotateY = 0;
+        const int rotateZ = 1;
+        const int rotateX = 2;
+      Vector3Int rotate(
         float theta, float alpha,
-        Vector3Int origin,Vector3Int point
+        Vector3Int origin,Vector3Int point,
+        int direction
          ){
-        float lineX = point.x-origin.x;
-        float lineY = point.y-origin.y;
-        float lineZ = point.z-origin.z;
-        float radius = MathF.Sqrt(
-            Mathf.Pow(lineX,2)+
-            Mathf.Pow(lineY,2)+
-            Mathf.Pow(lineZ,2)
-        );
-        float angleToRadian = Mathf.PI/180;
-        float currentTheta = Mathf.Asin(lineY/radius);
-        float adjacent = radius*Mathf.Cos(currentTheta);
-        float currentAlpha = Mathf.Acos(lineZ/adjacent);
+            float lineX = point.x-origin.x;
+            float lineY = point.y-origin.y;
+            float lineZ = point.z-origin.z;
+            float radius = MathF.Sqrt(
+                Mathf.Pow(lineX,2)+
+                Mathf.Pow(lineY,2)+
+                Mathf.Pow(lineZ,2)
+            );
+            float angleToRadian = Mathf.PI/180;
+            float currentTheta,adjacent,currentAlpha;
+            float x = 0;
+            float y = 0;
+            float z = 0;
+            switch(direction){
+                case rotateZ:
+                    currentTheta = Mathf.Asin(lineY/radius);
+                    adjacent = radius*Mathf.Cos(currentTheta);
+                    currentAlpha = Mathf.Acos(lineZ/adjacent);
+                    alpha = alpha*angleToRadian - currentAlpha;
+                    theta = theta*angleToRadian + currentTheta;
+                    x = adjacent*Mathf.Sin(alpha);
+                    z = adjacent*Mathf.Cos(alpha);
+                    y = radius*Mathf.Sin(theta);
+                break;
+                case rotateX:
+                    currentTheta = Mathf.Asin(lineZ/radius);
+                    adjacent = radius*Mathf.Cos(currentTheta);
+                    currentAlpha = Mathf.Acos(lineX/adjacent);
+                    alpha = alpha*angleToRadian + currentAlpha;
+                    theta = theta*angleToRadian + currentTheta;
+                    y = adjacent*Mathf.Sin(alpha);
+                    x = adjacent*Mathf.Cos(alpha);
+                    z = radius*Mathf.Sin(theta);
+                break;
+                case rotateY:
+                    currentTheta = Mathf.Asin(lineX/radius);
+                    adjacent = radius*Mathf.Cos(currentTheta);
+                    currentAlpha = Mathf.Acos(lineZ/adjacent);
+                    alpha = alpha*angleToRadian + currentAlpha;
+                    theta = theta*angleToRadian + currentTheta;
+                    y = adjacent*Mathf.Sin(alpha);
+                    z = adjacent*Mathf.Cos(alpha);
+                    x = radius*Mathf.Sin(theta);
+                break;
+            }
+            int x1 = (x>0)? (int)(x +0.5f):(int)(x -0.5f);
+            int y1 = (y>0)? (int)(y +0.5f):(int)(y -0.5f);
+            int z1 = (z>0)? (int)(z +0.5f):(int)(z -0.5f);
 
-        alpha = alpha*angleToRadian + currentAlpha;
-        theta = theta*angleToRadian + currentTheta;
-        float x = adjacent*Mathf.Sin(alpha);
-        float z = adjacent*Mathf.Cos(alpha);
-        float y = radius*Mathf.Sin(theta);
-
-        int x1 = (x>0)? (int)(x +0.5f):(int)(x -0.5f);
-        int y1 = (y>0)? (int)(y +0.5f):(int)(y -0.5f);
-        int z1 = (z>0)? (int)(z +0.5f):(int)(z -0.5f);
-        return new Vector3Int(x1,y1,z1);
-    } 
-
-    Vector3Int rotateXY(
-        float theta, float alpha,
-        Vector3Int origin,Vector3Int point
-         ){
-        float lineX = point.x-origin.x;
-        float lineY = point.y-origin.y;
-        float lineZ = point.z-origin.z;
-        float radius = MathF.Sqrt(
-            Mathf.Pow(lineX,2)+
-            Mathf.Pow(lineY,2)+
-            Mathf.Pow(lineZ,2)
-        );
-        float angleToRadian = Mathf.PI/180;
-        float currentTheta = Mathf.Asin(lineZ/radius);
-        float adjacent = radius*Mathf.Cos(currentTheta);
-        float currentAlpha = Mathf.Acos(lineX/adjacent);
-
-        alpha = alpha*angleToRadian + currentAlpha;
-        theta = theta*angleToRadian + currentTheta;
-        float y = adjacent*Mathf.Sin(alpha);
-        float x = adjacent*Mathf.Cos(alpha);
-        float z = radius*Mathf.Sin(theta);
-
-        int x1 = (x>0)? (int)(x +0.5f):(int)(x -0.5f);
-        int y1 = (y>0)? (int)(y +0.5f):(int)(y -0.5f);
-        int z1 = (z>0)? (int)(z +0.5f):(int)(z -0.5f);
-        return new Vector3Int(x1,y1,z1);
-    }
-
-    Vector3Int rotateYZ(
-        float theta, float alpha,
-        Vector3Int origin,Vector3Int point
-         ){
-        float lineX = point.x-origin.x;
-        float lineY = point.y-origin.y;
-        float lineZ = point.z-origin.z;
-        float radius = MathF.Sqrt(
-            Mathf.Pow(lineX,2)+
-            Mathf.Pow(lineY,2)+
-            Mathf.Pow(lineZ,2)
-        );
-        float angleToRadian = Mathf.PI/180;
-        float currentTheta = Mathf.Asin(lineX/radius);
-        float adjacent = radius*Mathf.Cos(currentTheta);
-        float currentAlpha = Mathf.Acos(lineZ/adjacent);
-
-        alpha = alpha*angleToRadian + currentAlpha;
-        theta = theta*angleToRadian + currentTheta;
-        float y = adjacent*Mathf.Sin(alpha);
-        float z = adjacent*Mathf.Cos(alpha);
-        float x = radius*Mathf.Sin(theta);
-
-        int x1 = (x>0)? (int)(x +0.5f):(int)(x -0.5f);
-        int y1 = (y>0)? (int)(y +0.5f):(int)(y -0.5f);
-        int z1 = (z>0)? (int)(z +0.5f):(int)(z -0.5f);
-        return new Vector3Int(x1,y1,z1);
-    }  
+            return new Vector3Int(x1,y1,z1);
+         }
     
     int l = -90;
     void Update(){
@@ -122,7 +95,7 @@ public class Body : MonoBehaviour
         if (i<360){
             WorldBuilder.createOrDelete(
                 WorldBuilder.setVectorInBoundry(
-                    chest[0], rotateYZ(0,0,chest[0],chest[1])
+                    chest[0], rotate(0,0,chest[0],chest[1],1)
                     ),true
                 );
                 i++;
