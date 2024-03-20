bcb940c7b39c0b7ecda510a9d624560824438efb
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-20 23:02:27 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -9,30 +9,27 @@ public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
     Vector3[] chest = new Vector3[]{
-         new Vector3(5.8f,5.2f,2.6f),new Vector3(2.6f,3.2f,8.8f)
+         new Vector3(8f,2f,8f),new Vector3(5f,10f,2f)
          };
     public static Vector3 move = new Vector3(6f,6f,6f); 
     public static Vector3[] tempChest;     
     void Start(){
         WorldBuilder.createOrDeleteObject(chest,true);
+        tempChest = WorldBuilder.diagonal(chest,0.05f);
     }
 
     // Update is called once per frame
-        float time = 0;
-        int i = 0;
-    
-    int l = 0;
+    float time = 0;
     void Update(){
         time += Time.deltaTime;
-        if (time >0.01f){
-        chest = WorldBuilder.moveObject(
-            new Vector3(0f,0f,1f),chest
-        );
-        chest = WorldBuilder.rotateObject(
-            0,1,WorldBuilder.rotateZ,move,chest
-        );
-        print($"{chest[0]} {chest[1]}");
+        if (time >0.1f){
         time = 0;
         }
     }
 }
+        // chest = WorldBuilder.moveObject(
+        //     new Vector3(0f,0f,-1f),chest
+        // );
+        // chest = WorldBuilder.rotateObject(
+        //     0,1,WorldBuilder.rotateZ,move,chest
+        // );
