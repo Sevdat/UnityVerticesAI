a8d4e7e94d66df5464d288d6901f4ace6685278d
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-22 01:48:37 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -10,7 +10,10 @@ public class Body : MonoBehaviour
     // Start is called before the first frame update
     float[] radius = new float[]{6,5,4};
     Vector3[] chest = new Vector3[]{
-         new Vector3(8f,2f,8f),new Vector3(5f,10f,2f)
+         new Vector3(10f,12f,10f),
+         new Vector3(10f,7f,10f),
+         new Vector3(10f,2f,10f),
+         new Vector3(10f,2f,13f)
          };
     public static Vector3 move = new Vector3(6f,6f,6f); 
     public static Vector3[] tempChest = new Vector3[]{
@@ -18,21 +21,21 @@ public class Body : MonoBehaviour
         };     
     void Start(){
         WorldBuilder.createOrDeleteObject(chest,true);
-        tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
+        // tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
     }
 
     // Update is called once per frame
     float time = 0;
     void Update(){
         time += Time.deltaTime;
-        if (time >0.05f){
-        chest = WorldBuilder.moveObject(
-            new Vector3(0f,0f,-1f),chest
+        if (time >0.5f){
+        // chest = WorldBuilder.moveObject(
+        //     new Vector3(0f,0f,-1f),chest
+        // );
+        WorldBuilder.rotateObject(
+            0,50,WorldBuilder.rotateY,chest[1],new Vector3[]{chest[2],chest[3]}
         );
-        chest = WorldBuilder.rotateObject(
-            0,1,WorldBuilder.rotateZ,chest[0],chest
-        );
-        tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
+        // tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
         time = 0;
         }
     }
