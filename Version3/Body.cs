08e4e705b81a64856b6fec39581370c8e5ff799e
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-03-23 13:04:29 +0100

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -9,6 +9,17 @@ public class Body : MonoBehaviour
 {
     // Start is called before the first frame update
     float[] radius = new float[]{6,5,4};
+    public struct bodyStructure{
+        public Vector3[] hip;
+        public Vector3[] knee;
+        public Vector3[] foot;
+        public void movehip(
+            Vector3[] hip
+            ){
+                knee = new Vector3[]{hip[1],hip[2],hip[3]};
+                foot = new Vector3[]{knee[1],knee[2]};
+        }
+    }
     Vector3[] chest = new Vector3[]{
          new Vector3(10f,12f,10f),
          new Vector3(10f,7f,10f),
@@ -19,23 +30,26 @@ public class Body : MonoBehaviour
     public static Vector3[] tempChest = new Vector3[]{
         new Vector3(0f,0f,0f)
         };     
+    bodyStructure lol;
     void Start(){
-        WorldBuilder.createOrDeleteObject(chest,true);
-        // tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
+        lol = new bodyStructure(){
+         hip = chest   
+        };
+        WorldBuilder.createOrDeleteObject(lol.hip,true);
+        lol.hip = WorldBuilder.rotateObject(
+            0,50,WorldBuilder.rotateY,lol.hip[0],lol.hip
+        );
     }
 
     // Update is called once per frame
     float time = 0;
     void Update(){
         time += Time.deltaTime;
-        if (time >0.5f){
-        // chest = WorldBuilder.moveObject(
-        //     new Vector3(0f,0f,-1f),chest
-        // );
-        WorldBuilder.rotateObject(
-            0,50,WorldBuilder.rotateY,chest[1],new Vector3[]{chest[2],chest[3]}
+        if (time >1f){
+        lol.movehip(lol.hip);
+        lol.hip = WorldBuilder.rotateObject(
+            0,50,WorldBuilder.rotateZ,lol.hip[0],lol.hip
         );
-        // tempChest = WorldBuilder.diagonal(chest,tempChest,0.05f);
         time = 0;
         }
     }
