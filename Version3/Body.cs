68afff476d2644ae337095be669e1a18a9ab2bb3
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-04-05 15:27:29 +0200

    Update Body.cs

--- Version3/Body.cs
+++ Version3/Body.cs
@@ -112,14 +112,18 @@ public class Body : MonoBehaviour
         angle = WorldBuilder.getAngles(origin,point);
         float l = circum(origin,point);
         point2 = WorldBuilder.rotate(
-            new Vector3(90,0,180),
+            new Vector3(0,-90,180),
             origin,
             point);
         draw = new Vector3[]{origin,point,point2};
         WorldBuilder.createOrDeleteObject(draw,true);
 
         print(WorldBuilder.getAngles(origin,point2));
-        rotate = new Vector3(-1,-1,2);
+        rotate = new Vector3(
+        0,
+        0,
+        0
+        );
     }
     // Update is called once per frame
     float time = 0;
