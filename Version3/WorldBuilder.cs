a878c2878629c2243c81cc6a4340101020c5c3b9
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-04-05 15:27:09 +0200

    Update WorldBuilder.cs

--- Version3/WorldBuilder.cs
+++ Version3/WorldBuilder.cs
@@ -226,6 +226,7 @@ public class WorldBuilder : MonoBehaviour
             float radius = vectorRadius(vectorDirection);
 
             float[] zValues = locatePoint(radius,lineY,lineZ,lineX);
+            if (alphaAngles.z > 180f) alphaAngles.z -= 180f;
             alpha = alphaAngles.z*angleToRadian + zValues[0];
             x = zValues[1]*Mathf.Sin(alpha);
             z = zValues[1]*Mathf.Cos(alpha);
@@ -236,6 +237,7 @@ public class WorldBuilder : MonoBehaviour
                 lineY = y;
                 lineZ = z;
                 float[] yValues = locatePoint(radius,lineX,lineY,lineZ);
+                if (alphaAngles.y > 180f) alphaAngles.y -= 180f;
                 alpha = alphaAngles.y*angleToRadian + yValues[0];
                 z = yValues[1]*Mathf.Sin(alpha);
                 y = yValues[1]*Mathf.Cos(alpha);
@@ -245,10 +247,11 @@ public class WorldBuilder : MonoBehaviour
                 lineX = x;
                 lineY = y;
                 lineZ = z;
-                float[] xValues = locatePoint(radius,lineZ,lineX,lineY);
+                float[] xValues = locatePoint(radius,lineZ,lineY,lineX);
+                if (alphaAngles.x > 180f) alphaAngles.x -= 180f;
                 alpha = alphaAngles.x*angleToRadian + xValues[0];
-                y = xValues[1]*Mathf.Sin(alpha);
-                x = xValues[1]*Mathf.Cos(alpha);
+                x = xValues[1]*Mathf.Sin(alpha);
+                y = xValues[1]*Mathf.Cos(alpha);
                 z = lineZ;
             }
             rotatedVec = origin + new Vector3(x,y,z);
