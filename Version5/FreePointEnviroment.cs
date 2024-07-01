b467f482bfccd0a9e0c709d1f59cd11401a2ef76
Sevdat <55320710+Sevdat@users.noreply.github.com>
2024-07-01 15:56:15 +0200

    Update FreePointEnviroment.cs

--- Version5/FreePointEnviroment.cs
+++ Version5/FreePointEnviroment.cs
@@ -8,30 +8,50 @@ public class FreePointEnviroment : MonoBehaviour
     public static GameObject dynamicClone;
     public static GameObject staticClone;
     public static GameObject cloneHierarchy;
+    public World world;
 
     public class World {
-        public Dictionary<Vector3Int,AtomicGrid> world;
-        public int worldSize,worldSizeLimit;
-            public class AtomicGrid {
-                public List<Joint> bodyList;
-                public int minIndexChange;
-            }
-    }
-
-    public class BodiesInWorld {
-        public Dictionary<string,Body> bodies;
-        public int bodyCount,bodyCountLimit;
-        public struct Body {
-            public List<Joint> jointList;
-            public int jointCount,jointCountLimit;
+        public int vector3Count,vector3CountLimit;
+        public Dictionary<string,List<Joint>> bodies;
+        public struct Joint{
+            public string name;
+            public int indexInWorldList;
+            public int indexInJointList,connectedTo;
+            public Vector3 position;
+            public Vector3 localX,localY,localZ;
+            public List<Vector3> meshShape;
         }
-    }
-    public struct Joint{
-        public string name,pathFromZero;
-        public int indexInWorldList,indexInJointList,connectedTo;
-        public Vector3 from,to;
-        public Vector3 x,y,z;
-        public List<Vector3> meshShape;
+        
+        public Dictionary<Vector3Int,AtomicGrid> collisionDetection;
+        public class AtomicGrid {
+            public List<Joint> bodyList;
+            public int minIndexChange;
+        }
+        public void body(string name,List<Joint>jointList){
+            if (!bodies.ContainsKey(name) && vector3Count<vector3CountLimit){
+                bodies[name] = jointList;
+            }
+        }
+        public Joint joint(
+                string name,
+                int indexInWorldList,
+                int indexInJointList,int connectedTo,
+                Vector3 position,
+                Vector3 localX,Vector3 localY,Vector3 localZ,
+                List<Vector3> meshShape
+                ){
+                    return new Joint(){
+                        name = name,
+                        indexInWorldList = indexInWorldList,
+                        indexInJointList = indexInJointList,
+                        connectedTo = connectedTo,
+                        position = position,
+                        localX = localX,
+                        localY = localY,
+                        localZ = localZ,
+                        meshShape = meshShape
+                    };
+        }
     }
     
     public static void createObject(string name,Vector3 vec){
@@ -51,7 +71,7 @@ public class FreePointEnviroment : MonoBehaviour
         staticClone = originalObject;
         staticClone.isStatic = true;
         createObject("lol",new Vector3(10,10,10));
-        // interpolate bewteewn skeleton joint points
+        world = new World();
     }
 
     // Update is called once per frame
